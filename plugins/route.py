81edc21d8ac053a6296e211a86983a2b6efc7c8c
oklife6 <119748274+oklife6@users.noreply.github.com>
2022-12-03 21:18:34 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -11,4 +11,4 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("CodeXBotz")
+    web.Response(text="Engine Vroom Vroom")
