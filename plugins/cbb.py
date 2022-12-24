09e8cb5bd29245ef567266ec2f202c961538bd73
oklife6 <119748274+oklife6@users.noreply.github.com>
2022-12-24 14:00:30 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Hosted on : <a href='fly.io'>Click here</a>\n○ Channel : https://t.me/cinemaxforall\n○ Support Group : https://t.me/+w3JL8EpLW-dhOTg9</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Hosted on : <a href='https://www.back4app.com/'>Click here</a>\n○ Channel : https://t.me/cinemaxforall\n○ Support Group : https://t.me/+w3JL8EpLW-dhOTg9</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
